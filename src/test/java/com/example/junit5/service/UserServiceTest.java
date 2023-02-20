7be53c755c31aca84d7dfd6ac23db4758a811042
Stanislav Korolkov <stasevich741@gmail.com>
2023-02-20 21:33:43 +0300

    Initial commit

---
+++ src/test/java/com/example/junit5/service/UserServiceTest.java
@@ -0,0 +1,13 @@
+package com.example.junit5.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class UserServiceTest {
+
+@Test
+    void test(){
+    assertTrue(true);
+}
+}
