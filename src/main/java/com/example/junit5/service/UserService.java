2022342b5963b379fc8264073504f7b6fd9f26f4
Stanislav Korolkov <stasevich741@gmail.com>
2023-02-21 13:58:40 +0300

    testing exceptions

--- src/main/java/com/example/junit5/service/UserService.java
+++ src/main/java/com/example/junit5/service/UserService.java
@@ -20,6 +20,8 @@ public class UserService {
     }
 
     public Optional<User> login(String username, String password) {
+        if (username == null || password == null)
+            throw new IllegalArgumentException("username or password is null");
 
         return users.stream()
                 .filter(user -> user.getUsername().equals(username))
